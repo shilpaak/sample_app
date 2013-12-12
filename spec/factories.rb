c7de2e5b441c7bb26816ba46951200711cdde3dc
shilpakondawar <shilpaak@github.com>
2013-12-12 17:15:15 +0530

    finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "shilpa"
+    email "kondawarsa@tw.com"
+    password "shilpa"
+    password_confirmation "shilpa"
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,10 +1,39 @@
 require 'spec_helper'
 
 describe "UserPages" do
-  subject{page}
+  subject { page }
   describe "Signup Page" do
     before { visit signup_path }
-      it {should have_content('Sign up')}
-      it { should have_title(full_title('Sign up')) }
+    it { should have_content('Sign up') }
+    it { should have_title(full_title('Sign up')) }
+
+  end
+  describe "Create new account" do
+    before{visit signup_path}
+    let(:submit){"Create my account"}
+    describe "with all blank data" do
+      it "should not create user" do
+        expect{click_button submit}.not_to change(User,:count)
+      end
     end
+    describe "with all valid data" do
+      before do
+        fill_in "Name", with: "shilpa kondawar"
+        fill_in "Email",        with: "shilpakondawar@gmail.com"
+        fill_in "Password",     with: "shilpa"
+        fill_in "Confirmation", with: "shilpa"
+      end
+      it "should create user" do
+        expect{click_button submit}.to change(User,:count).by(1)
+      end
+    end
+  end
+  describe "User Profile" do
+    let(:user) { FactoryGirl.create(:user) }
+    before {
+      visit user_path(user)
+    }
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
 end
